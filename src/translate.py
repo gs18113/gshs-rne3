38516376b162f9f4f0ce40c188860dbd79b3689a
gs18113 <seungmin960@gmail.com>
2020-03-12 04:24:28 +0900

    typo

--- src/translate.py
+++ src/translate.py
@@ -364,7 +364,7 @@ def train(train_data, val_data, source_vocab, target_vocab, source_serialize, ta
           for i in range(1, args.num_examples + 1):
             target_vocab_rev = {v:k for k, v in target_vocab}
             vocab_oovs_rev = {v+len(target_vocab)-1: k for k, v in val_set[i-1][4].items()}
-            target_vocab_extended_rev = (**target_vocab_rev, **vocab_oovs_rev)
+            target_vocab_extended_rev = {**target_vocab_rev, **vocab_oovs_rev}
             writer.add_text('Example/source_' + str(i), ''.join(tree2str(val_data[0]['source_ast'])), global_step = current_step)
             writer.add_text('Example/target_' + str(i), ''.join(tree2str(val_data[0]['target_ast'])), global_step = current_step)
             writer.add_text('Example/prediction_' + str(i), ''.join([target_vocab_extended_rev[id] for id in decoder_outputs[0]]), global_step = current_step)
