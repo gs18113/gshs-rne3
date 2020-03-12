3347601e34e126bb83376bbb1a98ea9349fc8689
gs18113 <seungmin960@gmail.com>
2020-03-12 23:27:31 +0900

    remote debugging

--- src/translate.py
+++ src/translate.py
@@ -1,3 +1,9 @@
+import ptvsd
+# Allow other computers to attach to ptvsd at this IP address and port.
+ptvsd.enable_attach(address=('127.0.0.1', 3000))
+# Pause the program until a remote debugger is attached
+ptvsd.wait_for_attach()
+
 import math
 import os
 import random
