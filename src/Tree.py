21fff7e698ba5e454b982b861de5b9996549cd2b
gs18113 <seungmin960@gmail.com>
2020-03-27 01:44:56 +0900

    scatter_add error fixed, testing...

--- src/Tree.py
+++ src/Tree.py
@@ -78,37 +78,37 @@ class TreeManager(object):
             lchild_id = self.create_binary_tree(data_utils.UNK_ID, current_id, depth + 1)
             self.trees[current_id].lchild = lchild_id
             return current_id
-        first_child_id = self.build_binary_tree_from_dict(init_tree['children'][0], current_id, depth + 1)
+        first_child_id = self.build_binary_tree_from_dict_oovs(init_tree['children'][0], current_id, depth + 1)
         self.trees[current_id].lchild = first_child_id
         pre_child_id = first_child_id
         for i in range(1, len(init_tree['children'])):
-            current_child_id = self.build_binary_tree_from_dict(init_tree['children'][i], pre_child_id, self.trees[pre_child_id].depth + 1)
+            current_child_id = self.build_binary_tree_from_dict_oovs(init_tree['children'][i], pre_child_id, self.trees[pre_child_id].depth + 1)
             self.trees[pre_child_id].rchild = current_child_id
             pre_child_id = current_child_id
         current_child_id = self.create_binary_tree(data_utils.UNK_ID, pre_child_id, self.trees[pre_child_id].depth + 1)
         self.trees[pre_child_id].rchild = current_child_id
         return current_id
 
-    def build_tree_from_dict(self, init_tree, target_len, parent=None, depth=0):
-        root = []
-        root.append(data_utils.GO_ID)
-        root.append(init_tree['root'])
-        children = []
-        current_id = self.create_tree(root, parent, depth)
-        for child in init_tree['children']:
-            root.append(data_utils.NT_ID)
-            child_id = self.build_tree_from_dict(child, target_len, current_id, depth + 1)
-            children.append(child_id)
-        root.append(data_utils.EOS_ID)
-        if len(root) < target_len:
-            root = root + [data_utils.PAD_ID] * (target_len - len(root))
-        root = Variable(torch.LongTensor(root))
-        if cuda.is_available():
-            root = root.cuda()
-        self.trees[current_id].root = root
-        self.trees[current_id].children = children
+    # def build_tree_from_dict(self, init_tree, target_len, parent=None, depth=0):
+    #     root = []
+    #     root.append(data_utils.GO_ID)
+    #     root.append(init_tree['root'])
+    #     children = []
+    #     current_id = self.create_tree(root, parent, depth)
+    #     for child in init_tree['children']:
+    #         root.append(data_utils.NT_ID)
+    #         child_id = self.build_tree_from_dict(child, target_len, current_id, depth + 1)
+    #         children.append(child_id)
+    #     root.append(data_utils.EOS_ID)
+    #     if len(root) < target_len:
+    #         root = root + [data_utils.PAD_ID] * (target_len - len(root))
+    #     root = Variable(torch.LongTensor(root))
+    #     if cuda.is_available():
+    #         root = root.cuda()
+    #     self.trees[current_id].root = root
+    #     self.trees[current_id].children = children
 
-        return current_id
+    #     return current_id
 
 def binarytree2str(prediction_manager, current_idx, vocab_rev):
     current_tree = prediction_manager.get_tree(current_idx)
